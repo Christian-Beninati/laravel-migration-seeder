f6f47bb2c701bd463f4ab62c0aa0971a96f9c8b7
Christian-Beninati <christianbeninati.info@gmail.com>
2023-08-23 20:21:05 +0100

    add TrainSeeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 30; $i++) {
+            $train = new Train();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->randomNumber(5, true);
+            $train->train_carriage = $faker->numberBetween(1, 30);
+            $train->in_time = $faker->boolean();
+            $train->deleted = $faker->boolean();
+            $train->save();
+        }
+    }
+}
